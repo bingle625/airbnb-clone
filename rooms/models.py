dd8e0362a0fb6d49fefd2f381cc4576d91361ffd
bingle625 <fhqhxm8899@gmail.com>
2022-07-13 11:00:59 +0900

    REFACTOR: Room Adnmin: list display, filter, edit view

--- rooms/models.py
+++ rooms/models.py
@@ -66,7 +66,7 @@ class Room(core_models.TimeStampedModel):
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
     amenities = models.ManyToManyField(Amenity, blank=True)
-    facility = models.ManyToManyField(Facility, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
     house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self) -> str:
